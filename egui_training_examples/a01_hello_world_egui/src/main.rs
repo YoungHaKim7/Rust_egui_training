8e95072421585cf22e785f9fe203352c97f943ff
GYoung <ytok1108@aol.com>
2025-07-08 19:33:39 +0900

    Hello world  font 30 이상 크기로

--- egui_training_examples/a01_hello_world_egui/src/main.rs
+++ egui_training_examples/a01_hello_world_egui/src/main.rs
@@ -9,47 +9,21 @@ fn main() -> Result<(), eframe::Error> {
         ..Default::default()
     };
     eframe::run_native(
-        "My egui App",
+        "Hello World",
         options,
-        Box::new(|cc| {
-            // This gives us image support:
-            egui_extras::install_image_loaders(&cc.egui_ctx);
-
-            Box::<MyApp>::default()
-        }),
+        Box::new(|_cc| Box::<MyApp>::default()),
     )
 }
 
-struct MyApp {
-    name: String,
-    age: u32,
-}
-
-impl Default for MyApp {
-    fn default() -> Self {
-        Self {
-            name: "Arthur".to_owned(),
-            age: 42,
-        }
-    }
-}
+#[derive(Default)]
+struct MyApp;
 
 impl eframe::App for MyApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading("My egui Application");
-            ui.horizontal(|ui| {
-                let name_label = ui.label("Your name: ");
-                ui.text_edit_singleline(&mut self.name)
-                    .labelled_by(name_label.id);
+            ui.centered_and_justified(|ui| {
+                ui.label(egui::RichText::new("Hello World!").size(30.0));
             });
-            ui.add(egui::Slider::new(&mut self.age, 0..=120).text("age"));
-            if ui.button("Click each year").clicked() {
-                self.age += 1;
-            }
-            ui.label(format!("Hello '{}', age {}", self.name, self.age));
-
-            ui.image("https://rustacean.net/assets/rustacean-orig-noshadow.svg");
         });
     }
 }
